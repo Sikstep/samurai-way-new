101c54f49d747e90687b9b2c123ed5bfc7198277
Sikstep <Sikstep1988@gmail.com>
2023-08-29 12:18:23 +0300

    create CustomSelect.tsx

--- src/CustomSelect.tsx
+++ src/CustomSelect.tsx
@@ -4,11 +4,41 @@ import styled from 'styled-components';
 export const CustomSelect = () => {
     return (
         <SelectWrapper>
-            <input type="text"/>
+            <div>
+                <InputStyledForCustomSelect/>
+                <ListStyled>content</ListStyled>
+            </div>
+            <ButtonStyledForCustomSelect>
+                up
+            </ButtonStyledForCustomSelect>
         </SelectWrapper>
     );
 };
 
 const SelectWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+`
 
+const InputStyledForCustomSelect = styled.input`
+  text-align: center;
+  border: none;
+  outline: none;
+  height: 4em;
+`
+
+const ButtonStyledForCustomSelect = styled.button`
+  height: 2em;
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  color: gray;
+`
+
+const ListStyled = styled.div`
+  position: absolute;
+  background-color: white;
+  width: 200px;
+  border-radius: 0 0 10px 10px;
 `
