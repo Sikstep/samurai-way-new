e770cd2917bc42b7c41a55b8029e7d75c4bc0014
Sikstep <Sikstep1988@gmail.com>
2023-08-29 10:22:21 +0300

    create LayOUtElement

---
+++ src/LayOut.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const LayOut = () => {
+    return (
+        <div>
+            <header>
+                header
+            </header>
+            <div>
+                content
+            </div>
+            <div>
+                void
+            </div>
+        </div>
+    );
+};
+
+const LayOutWrapper = styled.div`
+
+`
