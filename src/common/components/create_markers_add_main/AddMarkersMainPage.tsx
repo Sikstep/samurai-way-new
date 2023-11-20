fc1dfee11eb8edf191f5c63588c8c1a3e104215b
Sikstep <Sikstep1988@gmail.com>
2023-11-20 20:15:58 +0300

    fix slider bag

--- src/common/components/create_markers_add_main/AddMarkersMainPage.tsx
+++ src/common/components/create_markers_add_main/AddMarkersMainPage.tsx
@@ -69,6 +69,7 @@ export const AddMarkersMainPage = () => {
                                {...register('marker')}
                         />
                         <button type={'submit'}>
+
                             addMarker
                         </button>
                     </form>
